b1330c881e607ed8edca7a0b4e4810045639f457
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2025-02-14 13:29:59 -0800

    Refactor Trip Rendering Logic Into an Angular Component

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { trips } from '../data/trips';
 import { CommonModule } from '@angular/common';
+import { TripCardComponent } from '../trip-card/trip-card.component';
 
 @Component({
   selector: 'app-trip-listing',
-  imports: [CommonModule],
+  imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
