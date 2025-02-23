4e7db71a5b54dcb1f917ababe12b211f278ff136
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2025-02-22 23:18:52 -0800

    Implemented security protocol

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+var passport = require("passport");
+require("./app_api/config/passport");
 
 // Define routers
 var indexRouter = require("./app_server/routes/index");
@@ -34,16 +39,33 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+
+app.use((err, req, res, next) => {
+    if (err.name === "UnAuthorizedError") {
+        res.status(401).json({ message: err.name + ": " + err.message });
+    }
+});
 
 app.use("/api", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
+});
+
+app.use(passport.initialize());
+
+// catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ message: err.name + ": " + err.message });
+    }
 });
 
 app.use("/", indexRouter);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,20 +1,71 @@
 const express = require("express"); // Express app
 const router = express.Router(); // Router logic
 
+const jwt = require("jsonwebtoken");
+
 // This is where we import the controllers we will route
 const tripsController = require("../controllers/trips");
+const authController = require("../controllers/authentication");
+
+// register auth routes
+router.route("/login").post(authController.login);
+router.route("/register").post(authController.register);
+
+// define route for login endpoint
+router.route("/login").post(authController.login);
+
+// method to auth our JWT
+function authenticateJWT(req, res, next) {
+    const authHeader = req.headers["authorization"];
+
+    if (authHeader == null) {
+        console.log("Auth Header requireds but not present");
+        return res.sendStatus(401);
+    }
+
+    let headers = authHeader.split(" ");
+
+    if (headers.length < 1) {
+        console.log("Not enough tokens in auth header: " + headers.length);
+
+        return res.sendStatus(501);
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (token == null) {
+        console.log("Null bearer token");
+        return res.sendStatus(401);
+    }
+
+    const verified = jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+        (err, verified) => {
+            if (err) {
+                return res.sendStatus(401).json("Token validation error");
+            }
+            req.auth = verified;
+        }
+    );
+    next();
+}
 
 // define route for our trips endpoint
 router
     .route("/trips")
     .get(tripsController.tripsList) // GET method routes tripsList
-    .post(tripsController.tripsAddTrip); // POST method adds a Trip
+    .post(tripsController.tripsAddTrip) // POST method adds a Trip
+
+    .post(authenticateJWT, tripsController.tripsAddTrip);
 
 // GET method routes tripsByCode - requires parameter
 router
     .route("/trips/:tripCode")
     .get(tripsController.tripsFindByCode)
     .put(tripsController.tripsUpdateTrip)
-    .delete(tripsController.tripsDeleteTrip);
+    .delete(tripsController.tripsDeleteTrip)
+
+    .put(authenticateJWT, tripsController.tripsUpdateTrip);
 
 module.exports = router;
